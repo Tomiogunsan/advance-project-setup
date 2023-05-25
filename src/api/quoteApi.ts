eb352b573cf884fa2838d93298f26ab7899d4593
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-05-25 12:54:37 +0100

    feat: implemented updateQuote and fetchQuote using react-query

---
+++ src/api/quoteApi.ts
@@ -0,0 +1,14 @@
+import api from './api'
+
+export type Quote = {
+  id: string
+  quote: string
+  author: string
+}
+export type TopQuotesResponse = {
+  quotes: Quote[]
+}
+export const fetchTopQuotes = () =>
+  api.get<TopQuotesResponse>('quotes/top_quotes').then((res) => res.data.quotes)
+export const postQuote = (quote: Omit<Quote, 'id'>) => api.post('quotes', quote)
+ export const resetQuotes = () => api.post('quotes/reset', {})
