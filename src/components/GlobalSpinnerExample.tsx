65c7fb96675f031309a9bbf436e8e0b4e9a20a8b
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-05-29 11:24:55 +0100

    clean up

--- src/components/GlobalSpinnerExample.tsx
+++ src/components/GlobalSpinnerExample.tsx
@@ -1,34 +1,27 @@
-import {
-  GlobalSpinnerContext,
-  GlobalSpinnerContextValue,
-} from '@/context/GlobalSpinnerContext'
-import { useContext, memo } from 'react'
-type GlobalSpinnerExampleContentProps = { onShowSpinner: () => void }
-const GlobalSpinnerExampleContent = memo(
-  (props: GlobalSpinnerExampleContentProps) => {
-    console.log('GlobalSpinnerExample rendered')
-    return (
-      <div className='py-8 max-w-2xl mx-auto space-y-4'>
-        <button
-          className='bg-blue-600 text-blue-100 px-4 py-3'
-          onClick={props.onShowSpinner}
-        >
-          Show global spinner
-        </button>{' '}
-      </div>
-    )
-  },
-  (prevProps, nextProps) => true
-)
+import { useGlobalSpinnerActionsContext } from '@/context/GlobalSpinnerContext'
 type GlobalSpinnerExampleProps = {}
 const GlobalSpinnerExample = (props: GlobalSpinnerExampleProps) => {
-  const { showSpinner, hideSpinner } = useContext(
-    GlobalSpinnerContext
-  ) as GlobalSpinnerContextValue
+  const { showSpinner, hideSpinner } = useGlobalSpinnerActionsContext()
   const onShowSpinner = () => {
     showSpinner()
     setTimeout(hideSpinner, 2000)
   }
-  return <GlobalSpinnerExampleContent onShowSpinner={onShowSpinner} />
+  return (
+    <div className='py-8 max-w-2xl mx-auto space-y-4'>
+      <div className='leading-loose'>
+        The{' '}
+        <code className='bg-gray-200 p-1'>{'<GlobalSpinnerProvider />'}</code>{' '}
+        component is used in the App.tsx component. Thanks to that, the{' '}
+        <code className='bg-gray-200 p-1'>useGlobalSpinnerActionsContext</code>{' '}
+        method can be imported and used anywhere in the application.
+      </div>{' '}
+      <button
+        className='bg-blue-600 text-blue-100 px-4 py-3'
+        onClick={onShowSpinner}
+      >
+        Show global spinner
+      </button>{' '}
+    </div>
+  )
 }
 export default GlobalSpinnerExample
