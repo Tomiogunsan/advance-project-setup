f5664b7b915970f0d14e07977d87083b2b1978ee
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-06-01 11:23:31 +0100

    feat: normalizing state with createEntityAdapter

--- src/components/UsersManager/usersSlice.ts
+++ src/components/UsersManager/usersSlice.ts
@@ -5,13 +5,13 @@ import {
   PayloadAction,
   createSelector,
   createAsyncThunk,
+  createEntityAdapter,
 } from '@reduxjs/toolkit'
 import { User } from './UsersManager.types'
 
 type ApiStatus = 'IDLE' | 'PENDING' | 'SUCCESS' | 'ERROR'
 
 export type UsersState = {
-  users: User[]
   selectedUserId: User['id'] | null
   fetchUsersStatus: ApiStatus
   addUserStatus: ApiStatus
@@ -20,7 +20,6 @@ export type UsersState = {
 }
 
 const initialState: UsersState = {
-  users: [],
   selectedUserId: null,
   fetchUsersStatus: 'IDLE',
   addUserStatus: 'IDLE',
@@ -37,18 +36,16 @@ export const removeUserB = createAsyncThunk(
   }
 )
 
+const usersAdapter = createEntityAdapter<User>({
+  sortComparer: (a, b) => a.email.localeCompare(b.email),
+})
+
 export const usersSlice = createSlice({
   name: 'users',
-  initialState,
+  initialState: usersAdapter.getInitialState<UsersState>(initialState),
   reducers: {
     setUsers: (state, action: PayloadAction<User[]>) => {
-      state.users = action.payload
-    },
-    addUser: (state, action: PayloadAction<User>) => {
-      state.users.push(action.payload)
-    },
-    removeUser: (state, action: PayloadAction<User>) => {
-      state.users = state.users.filter((user) => user.id !== action.payload.id)
+      usersAdapter.setAll(state, action.payload)
     },
     selectUser: (state, action: PayloadAction<string>) => {
       state.selectedUserId = action.payload
@@ -60,7 +57,7 @@ export const usersSlice = createSlice({
     })
     builder.addCase(fetchUsers.fulfilled, (state, action) => {
       state.fetchUsersStatus = 'SUCCESS'
-      state.users = action.payload
+      usersAdapter.setAll(state, action.payload)
     })
     builder.addCase(fetchUsers.rejected, (state, action) => {
       state.fetchUsersStatus = 'ERROR'
@@ -69,38 +66,36 @@ export const usersSlice = createSlice({
       state.addUserStatus = 'PENDING'
     })
     builder.addCase(addUserB.fulfilled, (state, action) => {
-      state.users.push(action.payload.user)
+      usersAdapter.addOne(state, action.payload.user)
       state.addUserStatus = 'SUCCESS'
     })
     builder.addCase(addUserB.rejected, (state, action) => {
       state.addUserStatus = 'ERROR'
     })
-    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
     builder.addCase(removeUserB.pending, (state, action) => {
       state.deletingUserId = action.meta.arg.id
       state.deleteUserStatus = 'PENDING'
-    }),
-      builder.addCase(removeUserB.fulfilled, (state, action) => {
-        state.users = state.users.filter(
-          (_user) => _user.id !== action.payload.id
-        )
-        state.deleteUserStatus = 'SUCCESS'
-        state.deletingUserId = null
-      })
+    })
+    builder.addCase(removeUserB.fulfilled, (state, action) => {
+      usersAdapter.removeOne(state, action.payload.id)
+      state.deleteUserStatus = 'SUCCESS'
+      state.deletingUserId = null
+    })
     builder.addCase(removeUserB.rejected, (state, action) => {
       state.deleteUserStatus = 'ERROR'
       state.deletingUserId = null
     })
   },
 })
-export const { setUsers, selectUser, removeUser, addUser } = usersSlice.actions
-export const getSelectedUser = createSelector(
-  (state: RootState) => state.users,
-  (users) => {
-    if (users.selectedUserId) {
-      return users.users.find((user) => user.id === users.selectedUserId)
-    }
-    return null
-  }
+export const { setUsers, selectUser } = usersSlice.actions
+
+export const usersSelector = usersAdapter.getSelectors<RootState>(
+  (state) => state.users
 )
+export const getSelectedUser = (state: RootState) => {
+  return state.users.selectedUserId
+    ? usersSelector.selectById(state, state.users.selectedUserId)
+    : null
+}
+export const { selectAll: selectAllUsers } = usersSelector
 export default usersSlice.reducer
