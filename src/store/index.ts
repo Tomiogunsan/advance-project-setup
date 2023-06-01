49c068e7b0c45e1bca0d99eed71bd58bcae66a2e
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-06-01 12:41:25 +0100

    feat: testing reset function by creating another slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -11,9 +11,11 @@ import {
 } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import usersReducer from '@/components/UsersManager/usersSlice'
+import { counterReducer } from './counterSlice'
 
 const rootReducer = combineReducers({
   users: usersReducer,
+  counter: counterReducer,
 })
 
 const persistConfig = {
