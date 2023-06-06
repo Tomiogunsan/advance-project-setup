630f22569b2fccce61a9dcb0131f52e2b0f3ed88
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-06-06 18:40:47 +0100

    implemented dashboardLayout

--- src/App.tsx
+++ src/App.tsx
@@ -15,27 +15,48 @@ import TasksBoard from './components/TasksBoard'
 import ShoppingList from './components/ShoppingList'
 import UsersManager from './components/UsersManager/UsersManager'
 import DisplayBlogPosts from './components/hoc/DisplayBlogPosts'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Login from './views/auth/Login'
+import Register from './views/auth/Register'
+import Dashboard from './views/dashboard/Dashboard'
+
 
 const queryClient = new QueryClient()
 
 function App() {
+  
   return (
-    <GlobalSpinnerContextProvider>
-      <QueryClientProvider client={queryClient}>
-        <div className='App mx-auto max-w-6xl text-center my-8'>
-          {/* <AnimalExample /> */}
-          {/* <UpdateQuotes />
+    <BrowserRouter>
+      <GlobalSpinnerContextProvider>
+        <QueryClientProvider client={queryClient}>
+          <div className='App mx-auto max-w-6xl text-center my-8'>
+            {/* <AnimalExample /> */}
+            {/* <UpdateQuotes />
           <FetchTopQuotes />
           <PaginatedQuotes />
           <InfiniteScrollQuotes /> */}
-          {/* <GlobalSpinnerExample /> */}
-          {/* <TasksBoard />
+            {/* <GlobalSpinnerExample /> */}
+            {/* <TasksBoard />
           <ShoppingList /> */}
-          {/* <UsersManager /> */}
-          <DisplayBlogPosts />
-        </div>
-      </QueryClientProvider>
-    </GlobalSpinnerContextProvider>
+            {/* <UsersManager /> */}
+            <DisplayBlogPosts />
+          </div>
+          <nav className='my-8 space-x-4'>
+            {' '}
+            <Link to='/'>Dashboard</Link>
+            <Link to='/login'>Login</Link> 
+            <Link to='/register'>Register</Link>
+          </nav>
+          <div>
+            <Routes>
+              <Route path='/' element={<Dashboard />} />{' '}
+              <Route path='/login' element={<Login />} />{' '}
+              <Route path='/register' element={<Register />} />
+            </Routes>
+          </div>
+        </QueryClientProvider>
+      </GlobalSpinnerContextProvider>
+    </BrowserRouter>
   )
 }
 
